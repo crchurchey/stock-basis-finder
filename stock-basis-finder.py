9e9025db0e93e84ea3fccf41c403ff87876aa748
Christopher Churchey <chris.churchey@gmail.com>
2014-10-23 00:31:24 -0400

    Added logic to compute shares after dividends and splits

--- stock-basis-finder.py
+++ stock-basis-finder.py
@@ -3,6 +3,7 @@
 '''
 
 import argparse
+import datetime
 import logging
 import sys
 import traceback
@@ -16,7 +17,7 @@ LOG_FMT = '%(levelname)s: %(message)s'
 PHIST_DEFAULT = 'prices.csv'
 PHIST_DATE_KEY = 'Date'
 PHIST_PRICE_VAL = 'Close'
-PHIST_DATE_FMT = '%d-%b-%y'
+PHIST_DATE_FMT = '%Y-%m-%d'
 
 # Constants for Dividend History CSV
 DHIST_DEFAULT = 'dividends.csv'
@@ -42,7 +43,7 @@ if __name__ == "__main__":
     parser.add_argument('-df', '--dividend-file', metavar='<FILE>', type=str, default=DHIST_DEFAULT, help='CSV file that contains the dividend history for the investment (default=%s)' % DHIST_DEFAULT)
     parser.add_argument('-sf', '--split-file', metavar='<FILE>', type=str, default=SHIST_DEFAULT, help='CSV file that contains the split history for the investment (default=%s)' % SHIST_DEFAULT)
     parser.add_argument('--debug', action='store_true', default=False, help='Turn debug statements on or off')
-    parser.add_argument('-cs', '--cur-shares', metavar='<#######.##>', type=int, required=True, help='Number of shares of the investment currently owned')
+    parser.add_argument('-cs', '--cur-shares', metavar='<#######.##>', type=float, required=True, help='Number of shares of the investment currently owned')
 
     args = parser.parse_args()
 
@@ -95,7 +96,32 @@ if __name__ == "__main__":
     # loop over the dividends and splits
     sorted_date_keys = div_splits_dict.keys()
     sorted_date_keys.sort(reverse=True)
+    calc_shares = cur_shares
     for date_key in sorted_date_keys:
-        print 'date: %s' % date_key
-        print 'price_history: %d' % price_dict[date_key]
+        # Sometimes dividend pay dates may fall on weekends or holidays so search
+        # for the next price history if possible (within reason)
+        done = False
+        day_displacement = 0
+        while not done and day_displacement < 10:
+            tmp_key = date_key + datetime.timedelta(days=day_displacement)
+            try:
+                price = price_dict[tmp_key]
+                done = True
+            except KeyError:
+                # Add one to the day displacement and try again
+                day_displacement+=1
+        if day_displacement == 10:
+            raise Exception('Couldn\'t find the next valid price history date after %s', date_key)
 
+        # First check for dividends and process them first
+        if div_splits_dict[date_key].has_key(DIV_KEY):
+            div_amt = div_splits_dict[date_key][DIV_KEY]
+            cur_shares = (price * cur_shares) / (price + div_amt)
+
+        # Now see if there are splits to process
+        if div_splits_dict[date_key].has_key(SPLIT_KEY):
+            orig_shares = float(div_splits_dict[date_key][SPLIT_KEY][SHIST_OLD_SHARES])
+            new_shares = float(div_splits_dict[date_key][SPLIT_KEY][SHIST_NEW_SHARES])
+            cur_shares *= orig_shares / new_shares
+
+        print cur_shares
